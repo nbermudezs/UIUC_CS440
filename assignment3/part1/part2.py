9f245833a529bdd4f01d58f4c947ac93e26e5836
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-11-03 17:38:50 -0500

    Add entry point for part 1.2

---
+++ assignment3/part1/part2.py
@@ -0,0 +1,34 @@
+__author__ = 'Nestor Bermudez'
+__email__ = 'nab6@illinois.edu, nestor.bermudezs@gmail.com'
+
+from classifier import DigitClassifier
+from pixelGroupFeatureExtractor import PixelGroupFeatureExtractor
+from parser import Parser
+from util import Util
+
+if __name__ == '__main__':
+    import pdb
+
+    digitParser = Parser('digitdata/trainingimages', 'digitdata/traininglabels', 28)
+    extractor = PixelGroupFeatureExtractor(2, 3)
+    classifier = DigitClassifier(smoothing = 5)
+    classifier.train(extractor.items(digitParser.items()))
+
+    evaluationData = Parser('digitdata/testimages', 'digitdata/testlabels', 28)
+    confusion_matrix, acc = classifier.evaluate(extractor.items(evaluationData.items()))
+    Util.print_confusion_matrix(confusion_matrix, 10, 10)
+    print('Overall accuracy: ', round(acc * 100, 2))
+
+    labels = sorted(list(classifier.highest_likely_examples.keys()))
+    for label in labels:
+        features,_ = classifier.highest_likely_examples[ label ]
+        print('Highest likelihood for class: ', label)
+        Util.print_matrix(features, 28, 28)
+        print('\n')
+
+        features,_ = classifier.lowest_likely_examples[ label ]
+        print('Lowest likelihood for class: ', label)
+        Util.print_matrix(features, 28, 28)
+        print('\n\n')
+
+    pdb.set_trace()
