092bb7473ea581f8b9bf4d38e0219442a4e6a973
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-11-03 22:34:34 -0500

    Add utility functions to print out results

---
+++ assignment3/part2/util.py
@@ -0,0 +1,37 @@
+__author__ = 'Nestor Bermudez'
+__email__ = 'nab6@illinois.edu, nestor.bermudezs@gmail.com'
+
+class Util:
+    def print_matrix(matrix, rows, cols):
+        import math, sys
+        format = '%s'
+
+        for i in range(rows):
+            sep = '['
+            for j in range(cols):
+                val = matrix[ i ][ j ]
+                formatted = (format % val)
+                sys.stdout.write(sep + formatted)
+                sep = ', '
+            sys.stdout.write(']\n')
+
+    def print_confusion_matrix(matrix, rows, cols):
+        import math, sys
+        format = '%5s'
+
+        sep = '[   |'
+        for i in matrix.keys():
+            formatted = (format % i)
+            sys.stdout.write(sep + formatted)
+            sep = '| '
+        sys.stdout.write(']\n')
+
+        for i in matrix.keys():
+            row_sum = sum(matrix.get(i, {}).values())
+            sep = '[ ' + str(i) + ' |'
+            for j in matrix.keys():
+                val = matrix.get(i, {}).get(j, 0) / row_sum * 100.0
+                formatted = ('%5.1f' % val)
+                sys.stdout.write(sep + formatted)
+                sep = ', '
+            sys.stdout.write(']\n')
