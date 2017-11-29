1b071d5378b946d59b9625c2abd1af47298bfa7a
Néstor Bermúdez <nestor.bermudez@agilityfeat.com>
2017-11-29 13:57:28 -0600

    Add utility to plot the training curve

--- assignment4/part1/util.py
+++ assignment4/part1/util.py
@@ -2,8 +2,13 @@ __author__ = 'Nestor Bermudez'
 __email__ = 'nab6@illinois.edu, nestor.bermudezs@gmail.com'
 
 from queue import PriorityQueue
+import matplotlib.pyplot as plt
 
 class Util:
+    def plot_training_curve(data):
+        plt.scatter(range(len(data)), data, s=4)
+        plt.show()
+
     def print_matrix(matrix, rows, cols):
         import math, sys
         format = '%s'
